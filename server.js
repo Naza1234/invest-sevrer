6fc4bffd3b3c3fa55034b5c00aa38a57714b5454
naza-tech <ocbmf17@gmail.com>
2023-05-09 15:00:14 +0100

    just add cors
    cors

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const { json } = require("express");
+const cors = require('cors')
 
 
 
+
+app.use(cors({
+    origin : "http://127.0.0.1:5500"
+}))
 
 app.use(json());
 
